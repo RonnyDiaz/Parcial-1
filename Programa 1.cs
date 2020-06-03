6c496f9f032d7c212e0703dc89ccc868d3d0c48e
RonnyDiaz <65957665+RonnyDiaz@users.noreply.github.com>
2020-06-02 21:08:18 -0400

    Parcial

---
+++ Programa 1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Programa_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double producto;
+            string formapago;
+            double numerotarjeta;
+
+            Console.Write("Escriba el precio del producto: ");
+            producto = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Desea pagar efectivo o tarjeta ?: ");
+            formapago = Console.ReadLine();
+
+            if(formapago.Equals("tarjeta"))
+            {
+                Console.Write("Digite el numero de la tarjeta: ");
+                numerotarjeta = Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine("El precio del producto es {0}, el pago se realizo con la tarjeta de serie {1}", producto, numerotarjeta);
+            }
+            else
+            {
+                if(formapago.Equals("efectivo"))
+                {
+                    Console.WriteLine("Se ha pagado un monto de: {0}", producto);
+                }
+                else
+                {
+                    Console.WriteLine("La forma de pagar no es la adecuada");
+                }
+           
+            }
+
+        }
+    }
+}
---
+++ Programa 2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Programa_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+             string semana;
+
+            Console.WriteLine("Debe poner la primera letra en mayuscula...  Ej: Lunes");
+
+            Console.Write("Escriba un dia de la semana: "); 
+            semana = Console.ReadLine();
+
+           
+            switch (semana)
+            {
+                case "Lunes": Console.WriteLine("No es fin de semana"); break;
+                case "Martes": Console.WriteLine("No es fin de semana"); break;
+                case "Miercoles": Console.WriteLine("No es fin de semana"); break;
+                case "Jueves": Console.WriteLine("No es fin de semana"); break;
+                case "Viernes": Console.WriteLine("No es fin de semana"); break;
+                case "Sabado": Console.WriteLine("Es fin de semana"); break;
+                case "Domingo": Console.WriteLine("Es fin de semana"); break;
+
+                default: Console.WriteLine("Error"); break;
+
+            }
+           
+
+
+
+
+
+
+        }
+    }
+}
